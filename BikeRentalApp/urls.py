c107e1fb34d8fbabb8051aaa8ecf7035501e4f9e
rajish <maharjanrajish101@gmail.com>
2022-05-31 22:01:11 +0545

    CRUD

--- BikeRentalApp/urls.py
+++ BikeRentalApp/urls.py
@@ -3,9 +3,20 @@ from django.urls import path
 from .views import (
     UpdateProfile,
     ViewProfile,
+    CreateReadCategoryView,
+    UpdateCategoryView,
+    CreateReadVehicleView,
+    UpdatedDetailVehicleView
 )
 
 urlpatterns = [
     path('view_profile/', ViewProfile.as_view(),name = 'view-profile'),
     path('update_profile/', UpdateProfile.as_view(),name = 'update-profile'),
+    path('category/',CreateReadCategoryView.as_view(),name = 'category'),
+    path('update_category/<str:pk>/',UpdateCategoryView.as_view(),name = 'update-category'),
+
+    # in get method need vehicle id to get vehicles
+    # need category id to add vehicles
+    path('vehicle/<str:id>/',CreateReadVehicleView.as_view(),name = 'vehicle'),
+    path('update_vehicle/<str:pk>/',UpdatedDetailVehicleView.as_view(),name = 'update-vehicle')
 ]
--- BikeRentalProject/settings.py
+++ BikeRentalProject/settings.py
@@ -21,9 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# from decouple import config
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'django-insecure-q1!3wu#$9iy(iee!64%1t8e=8l&4am-=$aqh*sk25a9870qrkc'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
